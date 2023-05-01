5f1b406ce546a95cae6a16f22bfcef862feee2cf
dongha <shindh159@gmail.com>
2023-05-01 16:32:24 +0900

    init project

---
+++ src/main/kotlin/com/study/template/HexagonalKotlinTemplateApplication.kt
@@ -0,0 +1,11 @@
+package com.study.template
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HexagonalKotlinTemplateApplication
+
+fun main(args: Array<String>) {
+    runApplication<HexagonalKotlinTemplateApplication>(*args)
+}
